bb31da5a1c67dbcf1b207b10c0df1018a2e73783
Gleb Vassyutinskiy <g.v.vass@gmail.com>
2026-04-04 14:09:16 +0500

    resizables

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,48 @@
-import { Button } from "@/components/ui/button"
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <main className="h-svh w-full">
+      <ResizablePanelGroup
+        orientation="horizontal"
+        className="h-full w-full bg-card"
+      >
+        <ResizablePanel defaultSize={68} minSize={'30%'}>
+          {/* Canvas Component will be here */}
+          <div className="flex h-full items-center justify-center">
+            <p className="text-3xl font-semibold text-foreground">Content</p>
+          </div>
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+
+        <ResizablePanel defaultSize={32} minSize={'25%'}>
+          <ResizablePanelGroup orientation="vertical" className="h-full">
+            <ResizablePanel defaultSize={30} minSize={20}>
+              <div className="flex h-full items-center justify-center">
+                <p className="text-3xl font-semibold text-foreground">
+                  Call Info
+                </p>
+              </div>
+            </ResizablePanel>
+
+            <ResizableHandle withHandle />
+
+            <ResizablePanel defaultSize={70} minSize={20}>
+              <div className="flex h-full items-center justify-center">
+                <p className="text-3xl font-semibold text-foreground">
+                  Chat info
+                </p>
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </main>
   )
 }
 
